25e3dfc7db04c10fff903f71de7faf0d52ce5672
Стася <stasi-06@mail.ru>
2025-04-15 13:51:07 +0300

    Обновлён index.php: добавлен favicon и meta-тег для адаптивности

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <title>Главная</title>
     <link rel="stylesheet" href="/assets/css/style.css">
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+
 </head>
 <body>
     <?php include 'includes/header.php'; ?>
